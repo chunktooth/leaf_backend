a651bec5379be675ef78f3a1e13b774db8e1aa43
chunktooth <om.saetehn@gmail.com>
2018-06-05 10:18:38 -0600

    reseeded with plant morphology

--- db/seeds.rb
+++ db/seeds.rb
@@ -10,21 +10,207 @@ Plant.destroy_all
 
 user = User.create!(name: "Opaka", username: "O", password: "111")
 
-plants = [{
-  name: "Paper Birch",
-  sci_name: "Betula papyrifera",
-  img: "http://www.northernontarioflora.ca/images/descriptions/1000176_6.jpg", 
-  },
+plants = [
   {
-  name: "Quaking Aspen",
-  sci_name: "Populus tremuloides",
-  img: "https://newfs.s3.amazonaws.com/taxon-images-1000s1000/Salicaceae/populus-tremuloides-le-mlovit.jpg",
+    name: "Acicular",
+    img: "https://imgur.com/jRGfZO4"
   },
+
   {
-  name: "Rocky Mountain Juniper",
-  sci_name: "Juniperus scopulorum",
-  img: "https://www.crimson-sage.com/images/stories/virtuemart/product/rocky-mountain-juniper---juniperus-scopulorum.jpg",
-  }]
+    name: "Acuminate",
+    img: "https://imgur.com/8fbHnse" 
+  },
+
+  {
+    name: "Alternate",
+    img: "https://imgur.com/vUq0awP" 
+  },
+
+  {
+    name: "Aristate",
+    img: "https://imgur.com/CzWB2Rr" 
+  },
+
+  {
+    name: "Bipinnate",
+    img: "https://imgur.com/4l87yXI"
+  },
+
+  {
+    name: "Cordate",
+    img: "https://imgur.com/8rMzCtS"
+  },
+
+  {
+    name: "Cuneate",
+    img: "https://imgur.com/zrtQyxN"
+  },
+
+  {
+    name: "Deltoid",
+    img: "https://imgur.com/pg3npKU"
+  },
+
+  {
+    name: "Digitate",
+    img: "https://imgur.com/zjYbNit"
+  },
+
+  {
+    name: "Elliptic",
+    img: "https://imgur.com/3kz3eg6"
+  },
+
+  {
+    name: "Even-Pinnate",
+    img: "https://imgur.com/ONPHQVl",
+  },
+
+  {
+    name: "Falcate",
+    img: "https://imgur.com/W3lo09F"
+  },
+
+  {
+    name: "Flabellate",
+    img: "https://imgur.com/GLCAC6S"
+  },
+
+  {
+    name: "Hastate",
+    img: "https://imgur.com/swMY6VU"
+  },
+
+  {
+    name: "Lanceolate",
+    img: "https://imgur.com/YHNBIn2"
+  },
+
+  {
+    name: "Linear",
+    img: "https://imgur.com/BI27kJ4"
+  },
+
+  {
+    name: "Lobed",
+    img: "https://imgur.com/fQTwrUF"
+  },
+
+  {
+    name: "Obcordate",
+    img: "https://imgur.com/gcvBsk0"
+  },
+
+  {
+    name: "Obovate",
+    img: "https://imgur.com/Te4mJEz"
+  },
+
+  {
+    name: "Obtuse",
+    img: "https://imgur.com/lhlKkij"
+  },
+
+  {
+    name: "Odd-pinnate",
+    img: "https://imgur.com/inVBN65"
+  },
+
+  {
+    name: "Opposite",
+    img: "https://imgur.com/inVBN65"
+  },
+
+  {
+    name: "Orbicular",
+    img: "https://imgur.com/GQFDvHK"
+  },
+
+  {
+    name: "Ovate",
+    img: "https://imgur.com/ajEWEk0"
+  },
+
+  {
+    name: "Palmate",
+    img: "https://imgur.com/IwFrMr0"
+  },
+
+  {
+    name: "Pedate",
+    img: "https://imgur.com/Fire4n4"
+  },
+
+  {
+    name: "Pelate",
+    img: "https://imgur.com/MYbSoHa"
+  },
+
+  {
+    name: "Perfoliate",
+    img: "https://imgur.com/355IY0I"
+  },
+
+  {
+    name: "Pinnatisect",
+    img: "https://imgur.com/IBy6bQa"
+  },
+
+  {
+    name: "Reniform",
+    img: "https://imgur.com/GJp4gdl"
+  },
+
+  {
+    name: "Rhomboid",
+    img: "https://imgur.com/AHqrbZ3"
+  },
+
+  {
+    name: "Rosette",
+    img: "https://imgur.com/H2Fipx9"
+  },
+
+  {
+    name: "Spatulate",
+    img: "https://imgur.com/oXXGMvB"
+  },
+
+  {
+    name: "Spear-shaped",
+    img: "https://imgur.com/eNJyGLd"
+  },
+
+  {
+    name: "Subulate",
+    img: "https://imgur.com/By5V3j7"
+  },
+
+  {
+    name: "Trifoliate / Ternate",
+    img: "https://imgur.com/NVQ1HQb"
+  },
+
+  {
+    name: "Tripinnate",
+    img: "https://imgur.com/OAblk4D"
+  },
+
+  {
+    name: "Truncate",
+    img: "https://imgur.com/uFiYXA5"
+  },
+
+  {
+    name: "Unifoliate",
+    img: "https://imgur.com/z3mYTbn"
+  },
+
+  {
+    name: "Whorled",
+    img: "https://imgur.com/UeGoFmC"
+  },
+]
 
 plants.each do | plant |
   user.plants.create!(plant)
--- spec/models/plant_spec.rb
+++ spec/models/plant_spec.rb
@@ -2,7 +2,6 @@ require 'rails_helper'
 
 RSpec.describe Plant, type: :model do
  it { should validate_presence_of(:name) }
-  it { should validate_presence_of(:sci_name) }
   it { should validate_presence_of(:img) }
   it { should belong_to(:user) }
 end
