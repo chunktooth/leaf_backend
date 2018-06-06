1a445185f8817ad178b9b7afdf2e5df3898261f6
Luke Chambers <lukenatchambers@gmail.com>
2018-06-05 23:51:14 -0600

    Change to PG

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,7 +2,7 @@ class UsersController < ApplicationController
   before_action :set_user, only: [:show, :destroy]
 
   def index
-    @users = User.all
+    @users = User.where(username: params[:username])
     json_response(@users)
   end
 
